92e1b43bdf0aeb32d04d4ee75adab0f970116c13
a804 <36544044+a804@users.noreply.github.com>
2019-03-26 09:58:26 +0100

    Modificacion de fecha
    Para evitar la descordinacion de la aplicacion

--- music4all/es/uam/eps/padsof/music4all/Notificacion.java
+++ music4all/es/uam/eps/padsof/music4all/Notificacion.java
@@ -5,9 +5,9 @@ public class Notificacion {
 	private String mensaje;
 	private Date fecha;
 	
-	private Notificacion (String men) {
+	private Notificacion (String men, Date fec) {
 		this.mensaje = men;
-		this.fecha = new Date();
+		this.fecha = fec;
 	}
 	
 	public String getMensaje() {
